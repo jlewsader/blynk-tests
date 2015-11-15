e9cd643ed68ea9784894738acd93a6c713afdac7
Justin <jlewsader@gmail.com>
2015-11-15 11:27:45 -0500

    initial commit

---
+++ blynk.ino
@@ -0,0 +1,16 @@
+// This #include statement was automatically added by the Particle IDE.
+#include "~/Desktop/Dev/Particle Dev/Particle Librarys/blynk-library-spark/firmware/blynk.h"
+
+char auth[] = "765dc5e5e038459b952af4d138090e77";
+
+void setup()
+{
+  Serial.begin(9600); // See the connection status in Serial Monitor
+  delay(5000);        // Time to think
+  Blynk.begin(auth);  // Here you connect to the Blynk Cloud.
+}
+
+void loop()
+{
+  Blynk.run(); // All the Blynk Magic happens here...
+}
