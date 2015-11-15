8e8b29a1a004f686b22dc9cb7887613fd5789d84
Justin <jlewsader@gmail.com>
2015-11-15 12:19:52 -0500

    fix BLYNK_WRITE func inside Setup()

--- blynk.ino
+++ blynk.ino
@@ -10,7 +10,21 @@ void setup()
   Blynk.begin(auth);  // Here you connect to the Blynk Cloud.
 }
 
+BLYNK_WRITE(V0) {
+    int r = param[0].asInt(); // get a RED channel value
+    int g = param[1].asInt(); // get a GREEN channel value
+    int b = param[2].asInt(); // get a BLUE channel value
+    if (param[].asInt() > 0) {
+        RGB.control(true);
+        RGB.color(r,g,b);
+    } else {
+        RGB.control(false);
+    }
+}
+
 void loop()
 {
   Blynk.run(); // All the Blynk Magic happens here...
 }
+
+// THE END!
