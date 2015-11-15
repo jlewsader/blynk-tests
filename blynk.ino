406bee2b0039bdf076a998a9bbf763b013ca12b8
Justin <jlewsader@gmail.com>
2015-11-15 11:33:37 -0500

    fix include parh

--- blynk.ino
+++ blynk.ino
@@ -1,5 +1,5 @@
 // This #include statement was automatically added by the Particle IDE.
-#include "~/Desktop/Dev/Particle Dev/Particle Librarys/blynk-library-spark/firmware/blynk.h"
+#include "/firmware/blynk.h"
 
 char auth[] = "765dc5e5e038459b952af4d138090e77";
 
